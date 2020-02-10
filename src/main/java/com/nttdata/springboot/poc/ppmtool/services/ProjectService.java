95eea6e5235d097eb3bbdb50a17a62113cbb4f34
jameshughes7 <hugjimbo77@gmail.com>
2020-02-10 16:29:38 +0000

    wired up Project and ProjectController to save new project creation

---
+++ src/main/java/com/nttdata/springboot/poc/ppmtool/services/ProjectService.java
@@ -0,0 +1,21 @@
+package com.nttdata.springboot.poc.ppmtool.services;
+
+import com.nttdata.springboot.poc.ppmtool.domain.Project;
+import com.nttdata.springboot.poc.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+        //Logic
+
+        return projectRepository.save(project);
+    }
+
+}
