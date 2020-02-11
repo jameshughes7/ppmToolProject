3bea57a754c893c2b0f4732ef974c8812240b2d1
jameshughes7 <hugjimbo77@gmail.com>
2020-02-11 22:16:51 +0000

    custom exception handler created for ProjectIdentifier duplicate and refactored ProjectController into MapValidationErrorService

--- src/main/java/com/nttdata/springboot/poc/ppmtool/services/ProjectService.java
+++ src/main/java/com/nttdata/springboot/poc/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.nttdata.springboot.poc.ppmtool.services;
 
 import com.nttdata.springboot.poc.ppmtool.domain.Project;
+import com.nttdata.springboot.poc.ppmtool.exceptions.ProjectIdException;
 import com.nttdata.springboot.poc.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,13 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
-
-        return projectRepository.save(project);
+    public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 
 }
